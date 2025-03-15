40e8fcd1e722f1f8b731ae8598f9e782f7e1e45a
Guilherme Bracero Gonzales <guibragon@gmail.com>
2025-03-14 21:55:36 -0300

    add: Fonte Poppins & ref: Index Metricas

--- src/routes/Index.tsx
+++ src/routes/Index.tsx
@@ -3,6 +3,9 @@ import NavBar from "../components/NavBar";
 import IndexCards from "../components/IndexCards";
 import logo from "../assets/img/logos/AnnotaPs-Logo-Pequeno.png";
 import { useEffect } from "react";
+import { Button, Col, Container, Form, Row } from 'react-bootstrap';
+import figura1 from "../assets/img/etc/figura1.png";
+import figura2 from "../assets/img/etc/figura2.png";
 
 function Index() {
     useEffect(() => {
@@ -16,10 +19,33 @@ function Index() {
             </nav>
             
             <section>
-                <h1>Vistos Recentementes</h1>
+                <h1 className="text-center mb-4 titulo">Vistos Recentementes</h1>
 
                 <IndexCards />
                 
+                <div className="d-flex justify-content-center mt-4">
+                    <Button className="vermais">
+                        Ver mais
+                    </Button>
+                </div>
+            </section>
+
+            <section>
+                <div className="metricas-intro">
+                    <div className="figura" id="figura1">
+                        <img src={figura1} alt="" />
+                    </div>
+                    
+                    <div className="metricas-texto">
+                        <h1 className="titulo">Métricas do Setor</h1>
+                        <p>Acompanhe a contabilização das documentações e anotações realizadas pelo seu setor!</p>
+                    </div>
+
+                    <div className="figura" id="figura2">
+                        <img src={figura2} alt="" />
+                    </div>
+
+                </div>
             </section>
         </>
     )

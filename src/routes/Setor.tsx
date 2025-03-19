8a429216939894c241423f6e2de1519b96d7206a
Guilherme Bracero Gonzales <guibragon@gmail.com>
2025-03-18 21:30:33 -0300

    add: Barra de pesquisa em Setor.tsx

--- src/routes/Setor.tsx
+++ src/routes/Setor.tsx
@@ -1,6 +1,7 @@
 import NavBar from "../components/NavBar";
 import SetorInformacao from "../components/SetorInformacao";
 import logo from "../assets/img/logos/AnnotaPs-Logo-Pequeno.png";
+import "../assets/css/setor.css";
 
 import { useEffect } from "react";
 
@@ -12,7 +13,11 @@ function Setor () {
         <>
             <NavBar logoImage={logo} userImage={logo} />
             
-            <SetorInformacao />            
+            <SetorInformacao /> 
+
+            <section className="busca-container">
+                <input type="text" placeholder="Buscar por colaborador..."/>
+            </section>           
         </>
     )
 }

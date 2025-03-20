a3508040d741d1accbf2ec9fa3ed96382b8cd57b
Guilherme Bracero Gonzales <guibragon@gmail.com>
2025-03-20 07:43:11 -0300

    chore: Ambiente Backend criado e configurado

---
+++ Backend/core/settings/dev.py
@@ -0,0 +1,23 @@
+from .settings import *
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("S_KEY")
+
+DEBUG = os.getenv("DEBUG")
+
+ALLOWED_HOSTS = ['*']
+
+# docker run -p 5432:5432 -e POSTGRES_PASSWORD=1234 postgres
+DATABASES = {
+    "default": {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'AnnotaPS-Desenvolvimento',                      
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}

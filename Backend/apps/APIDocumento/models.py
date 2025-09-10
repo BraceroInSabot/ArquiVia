cd7e9857698bb508bb1058ef0916641a8209e4f1
BraceroInSabot <guibragon@gmail.com>
2025-09-10 11:08:47 -0300

    feat: Modelo Documento criado.

--- Backend/apps/APIDocumento/models.py
+++ Backend/apps/APIDocumento/models.py
@@ -1,3 +1,22 @@
+from apps.APISetor.models import Setor
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Documento(models.Model):
+    doc_id = models.AutoField(primary_key=True, db_column='ID_document')
+    title = models.CharField(max_length=200, db_column='document_title')
+    context_beta = models.TextField()
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, db_column='document_creator')
+    sector = models.ForeignKey(Setor, on_delete=models.CASCADE, db_column='document_sector')
+    data_criacao = models.DateTimeField(auto_now_add=True, db_column='creation_date')
+
+    def __str__(self):
+        return self.title 
+    
+    class Meta:
+        db_table = 'Document'
+        verbose_name = 'Document'
+        verbose_name_plural = 'Documents'

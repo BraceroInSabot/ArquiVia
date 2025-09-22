a4b8034a49582d5dbd02bd75706cc2682a22c9e6
BraceroInSabot <guibragon@gmail.com>
2025-09-22 10:55:54 -0300

    feat: Modelos Classification e Classification_Privacy criados

--- Backend/apps/APIDocumento/models.py
+++ Backend/apps/APIDocumento/models.py
@@ -21,3 +21,36 @@ class Document(models.Model):
         db_table = 'Document'
         verbose_name = 'Document'
         verbose_name_plural = 'Documents'
+        
+class Classification(models.Model):
+    classification_id = models.AutoField(primary_key=True, db_column='ID_classification')
+    reviewClassificationStatus = models.BooleanField(default=False, db_column='classification_review_status')
+    reviewedBy = models.ForeignKey(User, on_delete=models.CASCADE, db_column='classification_reviewed_by', related_name='reviewed_by_user')
+    document = models.ForeignKey(Document, on_delete=models.CASCADE, db_column='classification_document')
+    privacity = models.ForeignKey('Classification_Privacity', on_delete=models.CASCADE, db_column='classification_privacity', null=True)
+
+    def __str__(self):
+        return f"{self.document.title} - {self.reviewClassificationStatus}" 
+    
+    class Meta:
+        db_table = 'Classification'
+        verbose_name = 'Classification'
+        verbose_name_plural = 'Classifications'
+        
+class Classification_Privacity(models.Model):
+    privacity_choices = [
+        ('PB', 'Publico'),
+        ('PV', 'Privado'),
+    ]
+    
+    classification_privacity_id = models.AutoField(primary_key=True, db_column='ID_class_priv')
+    privacity = models.CharField(max_length=20, choices=privacity_choices, db_column='privacity')
+    priv_abreviation = models.CharField(max_length=2, default=None, null=True, db_column='abreviation')
+
+    def __str__(self):
+        return self.privacity 
+    
+    class Meta:
+        db_table = 'Classification_Privacity'
+        verbose_name = 'Classification Privacity'
+        verbose_name_plural = 'Classifications Privacies'

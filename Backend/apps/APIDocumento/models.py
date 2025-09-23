140cb90e9facf82b53d6453daa318a5442743931
BraceroInSabot <guibragon@gmail.com>
2025-09-23 11:32:20 -0300

    feat: Modelo Category criado

--- Backend/apps/APIDocumento/models.py
+++ Backend/apps/APIDocumento/models.py
@@ -61,7 +61,7 @@ class Classification_Privacity(models.Model):
     ]
     
     classification_privacity_id = models.AutoField(primary_key=True, db_column='ID_class_priv')
-    privacity = models.CharField(max_length=20, choices=privacity_choices, db_column='privacity')
+    privacity = models.CharField(max_length=20, choices=privacity_choices, db_column='privacity_name')
     priv_abreviation = models.CharField(max_length=2, default=None, null=True, db_column='abreviation')
 
     def __str__(self):
@@ -71,3 +71,16 @@ class Classification_Privacity(models.Model):
         db_table = 'Classification_Privacity'
         verbose_name = 'Classification Privacity'
         verbose_name_plural = 'Classifications Privacies'
+
+class Category(models.Model):
+    category_id = models.AutoField(primary_key=True, db_column='ID_category')
+    category = models.CharField(max_length=100, db_column='category_name')
+    description = models.TextField(db_column='category_description', null=True, blank=True)
+
+    def __str__(self):
+        return self.category 
+    
+    class Meta:
+        db_table = 'Category'
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'

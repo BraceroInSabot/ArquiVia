cc3f0ccae8552ac9d0eec190c186aae4fee5917d
BraceroInSabot <guibragon@gmail.com>
2025-09-23 09:14:49 -0300

    ref: Alteração do valor padrão do atributo eliminado pertencente ao modelo Documento

--- Backend/apps/APIDocumento/models.py
+++ Backend/apps/APIDocumento/models.py
@@ -12,7 +12,7 @@ class Document(models.Model):
     creator = models.ForeignKey(User, on_delete=models.CASCADE, db_column='document_creator')
     sector = models.ForeignKey(Sector, on_delete=models.CASCADE, db_column='document_sector')
     data_criacao = models.DateTimeField(auto_now_add=True, db_column='document_creation_date')
-    is_eliminate = models.BooleanField(default=True, db_column='document_active')
+    is_eliminate = models.BooleanField(default=False, db_column='document_active')
 
     def __str__(self):
         return self.title 

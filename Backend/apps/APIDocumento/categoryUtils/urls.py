3e644623095f7579adfac2fb7b8b93d22c945b1c
BraceroInSabot <guibragon@gmail.com>
2025-09-24 10:56:41 -0300

    feat: View para consulta de modelo Categoria

--- Backend/apps/APIDocumento/categoryUtils/urls.py
+++ Backend/apps/APIDocumento/categoryUtils/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CreateCategoryView
+from .views import CreateCategoryView, ShowCategoryView
 
 category_urlpatterns = [
     path('categoria/criar', CreateCategoryView.as_view(), name='criar-categoria'),
+    path('categoria/consultar', ShowCategoryView.as_view(), name='consultar-categoria'),
 ]
--- Backend/apps/APIDocumento/categoryUtils/views.py
+++ Backend/apps/APIDocumento/categoryUtils/views.py
@@ -100,3 +100,80 @@ class CreateCategoryView(APIView):
         }
         return ret
     
+class ShowCategoryView(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request):
+        category_id = request.data.get('category_id')
+        user = request.user
+        
+        try:
+            category: Type[Category] = Category.objects.get(category_id=category_id) #type: ignore
+        except Category.DoesNotExist:
+            ret = Response()
+            ret.status_code = 404
+            ret.data = {
+                "Data": {
+                    "sucesso": False,
+                    "mensagem": "Categoria não encontrada."
+                }
+            }
+            return ret
+
+        try:
+            vinculo: Type[SectorUser] = SectorUser.objects.filter(user=user, sector=category.category_sector).first() #type: ignore
+        except SectorUser.DoesNotExist:
+            ret = Response()
+            ret.status_code = 403
+            ret.data = {
+                "Data": {
+                    "sucesso": False,
+                    "mensagem": "Você não tem permissão para visualizar essa categoria."
+                }
+            }
+        
+        try:
+            if vinculo is None and category.category_sector.manager != user and Enterprise.objects.get(owner=user) != category.category_enterprise: #type: ignore
+                ret = Response()
+                ret.status_code = 403
+                ret.data = {
+                    "Data": {
+                        "sucesso": False,
+                        "mensagem": "Você não tem permissão para visualizar essa categoria."
+                    }
+                }
+                return ret
+        except:
+            ret = Response()
+            ret.status_code = 500
+            ret.data = {
+                "Data": {
+                    "sucesso": False,
+                    "mensagem": "Você não tem permissão para visualizar essa categoria."
+                }
+            }
+            return ret
+        
+        data = {
+            "category_id": category.category_id,
+            "titulo": category.category,
+            "descricao": category.description,
+            "setor": {
+                "sector_id": category.category_sector.sector_id,
+                "sector_name": category.category_sector.name
+            } if category.category_sector else None,
+            "empresa": {
+                "enterprise_id": category.category_enterprise.ent_id,
+                "enterprise_name": category.category_enterprise.name
+            } if category.category_enterprise else None
+        }
+        
+        ret = Response()
+        ret.status_code = 200
+        ret.data = {
+            "Data": {
+                "sucesso": True,
+                "mensagem": data
+            }
+        }
+        return ret

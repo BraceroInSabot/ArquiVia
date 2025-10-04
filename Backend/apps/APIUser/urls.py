4e79781886eefa1a6c9de3d777fffdc2c70798d7
BraceroInSabot <guibragon@gmail.com>
2025-10-04 19:32:56 -0300

    chore: Inclusão de teste unitário (pytest), arquivos de dependencias | ref: Ajuste no url do APIUser para adequar os testes

--- Backend/apps/APIUser/urls.py
+++ Backend/apps/APIUser/urls.py
@@ -14,10 +14,10 @@ from .views import (
 
 urlpatterns = [
     # Authentication
-    path("entrar", LoginTokenObtainPairView.as_view()),
-    path("atualizar-token", LoginTokenRefreshPairView.as_view()),
-    path("sair", LogoutTokenView.as_view()),
-    path("criar-conta", RegisterTokenView.as_view()),
+    path("entrar", LoginTokenObtainPairView.as_view(), name="entrar"),
+    path("atualizar-token", LoginTokenRefreshPairView.as_view(), name="atualizar-token"),
+    path("sair", LogoutTokenView.as_view(), name="sair"),
+    path("criar-conta", RegisterTokenView.as_view(), name="criar-conta"),
 ] + [
     # Password Reset
     # path("esqueci-senha", RequisicaoRedefinicaoSenhaView.as_view()),

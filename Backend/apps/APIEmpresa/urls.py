882f7ee926176ae27a1ebb702c768b1720fc9a73
BraceroInSabot <guibragon@gmail.com>
2025-10-08 15:10:32 -0300

    ref: Inserção de pk na url de Edição de Empresa para boas práticas

--- Backend/apps/APIEmpresa/urls.py
+++ Backend/apps/APIEmpresa/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path("criar", CreateEnterpriseView.as_view(), name="criar-empresa"),
     path("consultar/<int:pk>", RetrieveEnterpriseView.as_view(), name="consultar-empresa"),
     path("visualizar", ListEnterpriseView.as_view(), name="visualizar-empresas"),
-    path("alterar", EditEnterpriseView.as_view(), name="alterar-empresa"),
+    path("alterar/<int:pk>", EditEnterpriseView.as_view(), name="alterar-empresa"),
     path("ativar-desativar", ActivateOrDeactivateEnterpriseVIew.as_view(), name="ativar-desativar-empresa"),
     path("excluir", ExcludeEnterpriseView.as_view(), name="excluir-empresa"),
 ]

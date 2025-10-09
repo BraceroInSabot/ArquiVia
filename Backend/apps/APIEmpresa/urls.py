e6107d286bc9a9f28773a2da0211e754e8b0bdfe
BraceroInSabot <guibragon@gmail.com>
2025-10-09 09:31:37 -0300

    ref: Adição de PK na url

--- Backend/apps/APIEmpresa/urls.py
+++ Backend/apps/APIEmpresa/urls.py
@@ -14,6 +14,6 @@ urlpatterns = [
     path("consultar/<int:pk>", RetrieveEnterpriseView.as_view(), name="consultar-empresa"),
     path("visualizar", ListEnterpriseView.as_view(), name="visualizar-empresas"),
     path("alterar/<int:pk>", EditEnterpriseView.as_view(), name="alterar-empresa"),
-    path("ativar-desativar", ActivateOrDeactivateEnterpriseVIew.as_view(), name="ativar-desativar-empresa"),
+    path("ativar-desativar/<int:pk>", ActivateOrDeactivateEnterpriseVIew.as_view(), name="ativar-desativar-empresa"),
     path("excluir", ExcludeEnterpriseView.as_view(), name="excluir-empresa"),
 ]

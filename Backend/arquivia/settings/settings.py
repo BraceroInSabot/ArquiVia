9134adc5a55215959e1576a3270c36ee99a41b3c
BraceroInSabot <guibragon@gmail.com>
2025-10-16 16:31:13 -0300

    chore: Recolocação do carregamento do cors na lista de MIDDLEWARES

--- Backend/arquivia/settings/settings.py
+++ Backend/arquivia/settings/settings.py
@@ -32,6 +32,7 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -39,10 +40,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-] + [
-    "corsheaders.middleware.CorsMiddleware",
-    # "apps.APIUserAuth.utils.custom_middleware.PartitionedCookieMiddleware"
-]
+] 
 
 
 

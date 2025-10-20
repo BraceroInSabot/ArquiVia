755f915a7fc49adb606a40731d736053dc5faabc
BraceroInSabot <guibragon@gmail.com>
2025-10-20 15:22:21 -0300

    ref: Autorização de CORS para frontend local até o lançamento da prévia

--- Backend/arquivia/settings/prod.py
+++ Backend/arquivia/settings/prod.py
@@ -27,10 +27,14 @@ ALLOWED_HOSTS += [
 ]
 
 CORS_ALLOWED_ORIGINS += [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
     "https://arquivia.bracero.com.br",
 ]
 
 CSRF_TRUSTED_ORIGINS += [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
     "https://arquivia.bracero.com.br",
 ]
 

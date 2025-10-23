3536c0c102a76fdfdee294fb8f4c84e556567088
BraceroInSabot <guibragon@gmail.com>
2025-10-23 10:56:08 -0300

    ref: Refatoração da view de adição de usuário

--- Backend/apps/APISetor/sectorUserUtils/views.py
+++ Backend/apps/APISetor/sectorUserUtils/views.py
@@ -6,72 +6,41 @@ from rest_framework.permissions import IsAuthenticated
 from apps.APISetor.models import Sector, SectorUser
 from apps.APIEmpresa.models import Enterprise
 from django.contrib.auth import get_user_model
+from apps.APISetor.permissions import IsOwnerManagerOrSectorAdmin
+from apps.core.utils import default_response
 
 
 USER = get_user_model()
 
 class AddUserToSectorView(APIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsOwnerManagerOrSectorAdmin]
     
-    def post(self, request):
-        sector_id = request.data.get("sector_id")
+    def post(self, request, pk: int):
         user_email = request.data.get("user_email")
         
-        try:
-            sector: Sector = Sector.objects.filter(sector_id=sector_id).first()          
-        except Enterprise.DoesNotExist:
-            res = Response()
-            res.status_code=400
-            res.data = {"Data": 
-                        { 
-                            "sucesso": False,
-                            "mensagem": "Setor não encontrado. Tente novamente."
-                    }}
-            return res 
-
-        if sector.enterprise.owner != request.user and not SectorUser.objects.filter(sector=sector, user=request.user, is_adm=True).exists():
-            res = Response()
-            res.status_code=403
-            res.data = {"Data": 
-                        { 
-                            "sucesso": False,
-                            "mensagem": "Você não tem permissão para adicionar usuários a este setor."
-                    }}
+        if not user_email:
+            res: HttpResponse = Response()
+            res.status_code = 400
+            res.data = default_response(success=False, message="O campo 'email' é obrigatório.")
             return res
         
-        try:
-            user: UserModel = USER.objects.filter(email=user_email).first()
-        except USER.DoesNotExist:
-            res = Response()
+        sector_query: Union[Sector, None] = get_object_or_404(Sector, pk=pk)
+        user_query: Union[USER, None] = get_object_or_404(USER, email=user_email) #type: ignore
+        self.check_object_permissions(request, sector_query)
+        
+        if SectorUser.objects.filter(sector=sector_query, user=user_query).exists():
+            res: HttpResponse = Response()
             res.status_code=400
-            res.data = {"Data": 
-                        { 
-                            "sucesso": False,
-                            "mensagem": "Usuário não encontrado. Tente novamente."
-                    }}
+            res.data = default_response(success=False, message="Usuário já está vinculado a este setor.") 
             return res 
         
-        if SectorUser.objects.filter(sector=sector, user=user).exists():
-            res = Response()
-            res.status_code=400
-            res.data = {"Data": 
-                        { 
-                            "sucesso": False,
-                            "mensagem": "Usuário já está vinculado a este setor."
-                    }}
-            return res 
-        
-        sector_user = SectorUser(
-            sector=sector,
-            user=user
+        sector_user = SectorUser.objects.create(
+            sector=sector_query,
+            user=user_query,
+            is_adm=False
         )
-        sector_user.save()
 
-        res = Response()
-        res.status_code = 200
-        res.data = {"Data": 
-                { 
-                    "sucesso": True,
-                    "mensagem": "Usuário adicionado ao setor com sucesso."
-            }}
+        res: HttpResponse = Response()
+        res.status_code = 201
+        res.data = default_response(success=True, message="Usuário adicionado ao setor com sucesso.")
         return res

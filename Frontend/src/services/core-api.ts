de817dd11763c1d651cf2ee06b74a9d9a7304b7b
BraceroInSabot <guibragon@gmail.com>
2025-11-05 06:37:48 -0300

    chore: Alteração de fonte de requisição do front para o back

--- Frontend/src/services/core-api.ts
+++ Frontend/src/services/core-api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const BASE: string = "https://bracero.com.br/"
-const BASE: string = "http://127.0.0.1:8000/"
+const BASE: string = "https://bracero.com.br/"
+// const BASE: string = "http://127.0.0.1:8000/"
 const VERSION: string = "api/v2/"
 const URL: string = BASE + VERSION
 

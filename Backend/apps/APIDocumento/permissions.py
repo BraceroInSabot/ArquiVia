d2a28881e1354508592a91b2fb16093796974882
BraceroInSabot <guibragon@gmail.com>
2025-11-12 04:26:30 -0300

    add: Permissão de Anexo de Documento para usuário

--- Backend/apps/APIDocumento/permissions.py
+++ Backend/apps/APIDocumento/permissions.py
@@ -1,5 +1,5 @@
 from rest_framework.permissions import BasePermission
-from apps.APISetor.models import SectorUser
+from apps.APISetor.models import Sector, SectorUser
 
 class IsLinkedToDocument(BasePermission):
     """
@@ -31,3 +31,35 @@ class IsLinkedToDocument(BasePermission):
             return True
 
         return False
+    
+class CanAttachDocument(BasePermission):
+    """
+    Permissão que concede acesso se o usuário for:
+    1. O dono da empresa do setor.
+    2. O gerente do setor.
+    3. Um membro do setor.
+    4. Se o documento for público, verifica se o usuário pertence à empresa.
+    """
+    
+    message = "Você não tem permissão para anexar arquivos a este documento."
+    
+    def has_object_permission(self, request, view, obj):
+        user = request.user
+        
+        is_owner = obj.sector.enterprise.owner == user
+        is_manager = obj.sector.manager == user
+        is_sector_member = obj.sector.sector_links.filter(user=user).exists()
+        
+        if obj.classification.privacity.privacity == 'Público':
+            document_enterprise = obj.sector.enterprise
+            member_link = SectorUser.objects.filter(user=user, sector__enterprise=document_enterprise).exists()
+            manager_link = Sector.objects.filter(manager=user, enterprise=document_enterprise).exists()
+            owner_link = Sector.objects.filter(enterprise__owner=user, enterprise=document_enterprise).exists()
+            
+            is_enterprise_member = member_link or manager_link or owner_link
+        
+        if is_owner or is_manager or is_sector_member or is_enterprise_member:
+            return True
+        
+        return False
+    

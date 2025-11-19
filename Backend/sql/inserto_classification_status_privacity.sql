408e2ef4ba1439e36817a4869e47425e0306dd45
BraceroInSabot <guibragon@gmail.com>
2025-11-19 00:21:25 -0300

    ref: Alteração da query padrão para adequar-se a sintaxe do postgres

--- Backend/sql/inserto_classification_status_privacity.sql
+++ Backend/sql/inserto_classification_status_privacity.sql
@@ -1,8 +1,8 @@
 -- database: C:\Users\Bracero\Documents\Faculdade\TCC\ArquiVia\Backend\arquivia\db.sqlite3
-insert into Classification_Privacity('PK_classification_privacity', 'privacity_classification_privacity') values (1, 'Privado');
-insert into Classification_Privacity('PK_classification_privacity', 'privacity_classification_privacity') values (2, 'Público');
+insert into "Classification_Privacity"("PK_classification_privacity", "privacity_classification_privacity") values (1, 'Privado');
+insert into "Classification_Privacity"("PK_classification_privacity", "privacity_classification_privacity") values (2, 'Público');
 
-insert into Classification_Status('PK_status', 'status') values (1, 'Concluído');
-insert into Classification_Status('PK_status', 'status') values (2, 'Em andamento');
-insert into Classification_Status('PK_status', 'status') values (3, 'Revisão necessária');
-insert into Classification_Status('PK_status', 'status') values (4, 'Arquivado');
+insert into "Classification_Status"("PK_status", "status") values (1, 'Concluído');
+insert into "Classification_Status"("PK_status", "status") values (2, 'Em andamento');
+insert into "Classification_Status"("PK_status", "status") values (3, 'Revisão necessária');
+insert into "Classification_Status"("PK_status", "status") values (4, 'Arquivado');

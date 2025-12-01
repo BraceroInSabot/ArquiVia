fa1eb87818caa08574ba4ec0e75513e961cd0917
Guilherme Bracero <guibragon@gmail.com>
2025-12-01 04:48:19 -0300

    chore (EmailSettings): Email configuration for production

--- Backend/arquivia/settings/prod.py
+++ Backend/arquivia/settings/prod.py
@@ -29,6 +29,16 @@ DATABASES = {
     )
 }
 
+# Non Comercial Purposes
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_PASSWORD")
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
 ALLOWED_HOSTS += [
     "bracero.com.br",
 ]
